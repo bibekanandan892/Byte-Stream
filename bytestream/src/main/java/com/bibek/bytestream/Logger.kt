44f4bad6c04a8ee594556871b4499bfba19710ca
bibekananda.n <bibekananda.n@iserveu.co.in>
2024-11-15 12:21:22 +0530

    Added documentation in Logger

--- bytestream/src/main/java/com/bibek/bytestream/Logger.kt
+++ bytestream/src/main/java/com/bibek/bytestream/Logger.kt
@@ -1,10 +1,25 @@
 package com.bibek.bytestream
 
+/**
+ * Interface representing a Logger, which provides a mechanism to log messages
+ * with different severity levels. The `log` method allows specifying a custom tag,
+ * message, throwable, and log type.
+ */
 interface Logger {
+
     companion object {
+        /** Default tag for logging messages */
         const val TAG = "ByteStream Log"
     }
 
+    /**
+     * Logs a message with the specified parameters.
+     *
+     * @param tag Optional tag to use for the log message. Defaults to [TAG].
+     * @param message The message to log. Defaults to an empty string.
+     * @param throwable An optional [Throwable] to log with the message. Defaults to null.
+     * @param type The type of log message, indicating severity. Defaults to [LogType.Debug].
+     */
     fun log(
         tag: String? = TAG,
         message: String? = "",
@@ -13,10 +28,23 @@ interface Logger {
     )
 }
 
+/**
+ * Sealed interface representing the different types of log messages, indicating
+ * the severity of the log.
+ */
 sealed interface LogType {
+    /** Log message type for verbose logging. */
     data object Verbose : LogType
+
+    /** Log message type for debug-level logging. */
     data object Debug : LogType
+
+    /** Log message type for informational messages. */
     data object Info : LogType
+
+    /** Log message type for warning messages. */
     data object Warn : LogType
+
+    /** Log message type for error messages. */
     data object Error : LogType
 }
