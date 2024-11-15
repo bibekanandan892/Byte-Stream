0917dbc7be10091f582d6ed3c05b2a7009316df9
bibekananda.n <bibekananda.n@iserveu.co.in>
2024-11-15 12:17:20 +0530

    Added documentation in DownloadDatabase

--- bytestream/src/main/java/com/bibek/bytestream/internal/database/DownloadDatabase.kt
+++ bytestream/src/main/java/com/bibek/bytestream/internal/database/DownloadDatabase.kt
@@ -3,8 +3,23 @@ package com.bibek.bytestream.internal.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-
+/**
+ * The Room database that holds the data for downloads.
+ *
+ * This database contains the [DownloadEntity] table, which stores information about
+ * individual downloads. The database version is set to 1.
+ *
+ * @see DownloadEntity The entity representing a download.
+ * @see DownloadDao The DAO (Data Access Object) that provides methods to interact with
+ * the database.
+ */
 @Database(entities = [DownloadEntity::class], version = 1, exportSchema = false)
 internal abstract class DownloadDatabase : RoomDatabase() {
+    /**
+     * Provides access to the [DownloadDao], which contains the methods for performing
+     * database operations on the download data.
+     *
+     * @return An instance of [DownloadDao].
+     */
     abstract fun downloadDao(): DownloadDao
 }
