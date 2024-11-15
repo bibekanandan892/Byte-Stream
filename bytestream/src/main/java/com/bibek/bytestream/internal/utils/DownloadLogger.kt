6dc31637f3149397a7dd29aeaef132b78c4fe925
bibekananda.n <bibekananda.n@iserveu.co.in>
2024-11-15 10:49:56 +0530

    Added documentation in download logger

--- bytestream/src/main/java/com/bibek/bytestream/internal/utils/DownloadLogger.kt
+++ bytestream/src/main/java/com/bibek/bytestream/internal/utils/DownloadLogger.kt
@@ -4,7 +4,21 @@ import android.util.Log
 import com.bibek.bytestream.LogType
 import com.bibek.bytestream.Logger
 
+/**
+ * A logger class for handling different log types with conditional logging.
+ *
+ * @property enableLogs Boolean flag to enable or disable logging.
+ */
 internal class DownloadLogger(private val enableLogs: Boolean) : Logger {
+
+    /**
+     * Logs a message with the specified log type, if logging is enabled.
+     *
+     * @param tag Optional tag for the log message, typically used to indicate the source of the log.
+     * @param message The message to log.
+     * @param throwable An optional Throwable to log for error or warning cases.
+     * @param type The type of log to use, which determines the severity level.
+     */
     override fun log(tag: String?, message: String?, throwable: Throwable?, type: LogType) {
         if (enableLogs) {
             when (type) {
